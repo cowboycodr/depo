d2853fd71de9501e1293507dff6623b329118cd4
Kian McKenna <kian.p.m.007@gmail.com>
2026-05-06 18:57:26 -0500

    Tighten file viewer gutter styling

--- apps/web/src/lib/FileViewer.svelte
+++ apps/web/src/lib/FileViewer.svelte
@@ -15,11 +15,11 @@
 </script>
 
 <div class="h-full w-full min-w-0 overflow-auto bg-surface-muted font-mono text-code leading-code">
-  <div class="min-w-max py-2">
+  <div class="min-w-max">
     {#each lines as line, index}
-      <div class="grid grid-cols-[4.25rem_minmax(0,1fr)] px-0">
-        <span class="select-none pr-4 text-right text-fg-ref">{index + 1}</span>
-        <code class="whitespace-pre pr-6 text-fg">{line || ' '}</code>
+      <div class="grid grid-cols-[3rem_minmax(0,1fr)]">
+        <span class="select-none pr-3 text-right text-fg-ref">{index + 1}</span>
+        <code class="whitespace-pre pr-4 text-fg">{line || ' '}</code>
       </div>
     {/each}
   </div>
